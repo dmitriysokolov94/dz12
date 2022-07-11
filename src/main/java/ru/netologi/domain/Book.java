ebcf25cea86f4b321c44eea5d62ca63297fd3e41
dmitriysokolov94 <ipsokolov2021@gmail.com>
2022-07-11 11:52:27 +0300

    second commit

--- src/main/java/ru/netologi/domain/Book.java
+++
@@ -1,4 +0,0 @@
-package ru.netologi.domain;
-
-public class Book {
-}
--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -1,4 +1,47 @@
 package ru.netology.manager;
 
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
 public class ProductManager {
+
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repo) {
+        this.repository = repo;
+    }
+
+    public void add(Product product) {
+        repository.save(product);
+    }
+
+    public Product[] findAll() {
+        Product[] products = repository.findAll();
+        return products;
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getTitle().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
